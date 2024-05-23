110f29d9f553fcd668f2a6375a846cd52e32c52b
Avk <aviel.shnaider@tutanota.com>
2024-05-23 20:15:29 +0300

    test

--- server.js
+++ server.js
@@ -5,41 +5,23 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-//enabling to work with all localhosts.
 app.use(cors());
 
-//const hostName = "www.nrg-center.co.il";
-const port = 4000;
+const port = 3333;
 
 app.post("/submit-recipt", async (req, res) => {
-  // need to check the number
-
   const number = req.body.number;
-  console.log(number);
 
-  console.log("submit workd server side ");
+  //TODO: save number db
 
-  fs.writeFile("formNumbers.txt", `${number}\n`, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("datasaved");
-  });
   res.send("number as been updated ");
 });
 
-/// other function
 app.get("/get-number", async (req, res) => {
-  console.log("hi im getting number");
-  fs.readFile("formNumbers.txt", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading file:", err);
-      return;
-    }
-    console.log(data);
-    res.send(data);
-  });
+  //TODO: read numbers from db
+  res.send([]);
 });
+
 app.listen(port, () => {
-  console.log(`Server running at `);
+  console.log(`Server running on port ${port}`);
 });
